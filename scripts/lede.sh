a1858bf5b134795d10177003bcd845ba8ccb5a77
leafxdd <69914703+leafxdd@users.noreply.github.com>
2024-11-12 19:00:19 +0800

    Update lede.sh

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -8,56 +8,12 @@
 # Blog: https://mlapp.cn
 #=================================================
 # Clone community packages to package/community
-
-mkdir package/community
-pushd package/community
-
-# Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
-rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
-rm -rf openwrt-package/verysync
-rm -rf openwrt-package/luci-app-verysync
-
-# apppppppp
-git clone --depth=1 https://github.com/leafxdd/dhdaxcw-app
-git clone --depth=1 -b beta https://github.com/rufengsuixing/luci-app-adguardhome
-
-
-# Add luci-app-ssr-plus
-git clone --depth=1 https://github.com/fw876/helloworld
-
-# Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-
-# Add OpenClash
-git clone --depth=1 -b dev https://github.com/vernesong/OpenClash
-
-# Add luci-app-poweroff
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
-
-# Add luci-theme
-rm -rf ../../customfeeds/luci/themes/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-git clone https://github.com/DHDAXCW/theme
-
-# Add luci-app-lucky
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky
-
-# alist
-git clone --depth=1 https://github.com/sbwml/luci-app-alist
-
-# Add OpenAppFilter
-git clone --depth=1 https://github.com/destan19/OpenAppFilter
-
 popd
  
 # Mod zzz-default-settings
 pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-sed -i '/18.06/d' zzz-default-settings
+# sed -i '/http/d' zzz-default-settings
+# sed -i '/18.06/d' zzz-default-settings
 export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
